0bca5f84744dfcd124eb613aa4b22eedcee30902
Julia Kacahnova <juliaandorra.mail.ru>
2025-05-26 17:13:34 +0300

    сделана задача 2.1

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,16 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  return [...arr].sort((a, b) => {
+    const aLower = a.toLowerCase();
+    const bLower = b.toLowerCase();
+    
+    const comparison = aLower.localeCompare(bLower, 'ru');
+    
+    if (comparison === 0) {
+      return a.localeCompare(b, 'ru', { caseFirst: 'upper' });
+    }
+    
+    return param === 'asc' ? comparison : -comparison;
+  });
 }
