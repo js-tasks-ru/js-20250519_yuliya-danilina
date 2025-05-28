65098651bed090d798633f66d768564f88dacf0b
Julia Kacahnova <juliaandorra.mail.ru>
2025-05-28 19:33:44 +0300

    сделана задача 3.3

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,21 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    if (size === undefined) {
+        return string;
+    }
 
+    if (typeof string !== 'string' || typeof size !== 'number' || size <= 0) return '';
+  
+    let result = '';
+    let count = 0;
+  
+    for (let i = 0; i < string.length; i++) {
+        count = (string[i] === string[i-1]) ? count + 1 : 1;
+        if (count <= size) result += string[i];
+    }
+  
+    return result;
 }
+
+console.log(trimSymbols('xxxaaaaab', 3));
